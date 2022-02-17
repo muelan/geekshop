7c29a66c3e42fb0bfbe01a688168360a802b226d
muelan <muelan@mail.ru>
2022-02-17 17:18:25 +0300

    login and registration added

--- geekshop/settings.py
+++ geekshop/settings.py
@@ -11,9 +11,6 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
-import _locale
-
-_locale._getdefaultlocale = (lambda *args: ['en_US', 'utf8'])
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
