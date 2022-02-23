be28d8c22d5bacb5cfef98d9fe81cb1e3414a547
muelan <muelan@mail.ru>
2022-02-23 23:27:12 +0300

    basket.quantity <= product.quantity Attempt...

--- baskets/views.py
+++ baskets/views.py
@@ -7,14 +7,18 @@ from baskets.models import Basket
 def basket_add(request, product_id):
     product = Product.objects.get(id=product_id)
     baskets = Basket.objects.filter(user=request.user, product=product)
-
-    if not baskets:
-        Basket.objects.create(user=request.user, product=product, quantity=1)
-        return HttpResponseRedirect(request.META['HTTP_REFERER'])
+    if product.quantity > 0:
+        if not baskets:
+            Basket.objects.create(user=request.user, product=product, quantity=1)
+            product.quantity -= 1
+            return HttpResponseRedirect(request.META['HTTP_REFERER'])
+        else:
+            basket = baskets.first()
+            basket.quantity += 1
+            product.quantity -= 1
+            basket.save()
+            return HttpResponseRedirect(request.META['HTTP_REFERER'])
     else:
-        basket = baskets.first()
-        basket.quantity += 1
-        basket.save()
         return HttpResponseRedirect(request.META['HTTP_REFERER'])
 
 
