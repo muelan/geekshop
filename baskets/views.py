22ec4491ea68930eddd81f814ed1d9e0e695f1de
muelan <muelan@mail.ru>
2022-03-02 19:41:07 +0300

    BasketAdminInline added My bug in basket_add() fixed

--- baskets/views.py
+++ baskets/views.py
@@ -9,19 +9,17 @@ from baskets.models import Basket
 
 @login_required
 def basket_add(request, product_id):
-    if request.is_ajax():
-        product = Product.objects.get(id=product_id)
-        baskets = Basket.objects.filter(user=request.user, product=product)
+    product = Product.objects.get(id=product_id)
+    baskets = Basket.objects.filter(user=request.user, product=product)
 
-        if not baskets:
-            Basket.objects.create(user=request.user, product=product, quantity=1)
-        else:
-            basket = baskets.first()
-            basket.quantity += 1
-            basket.save()
-        context = {'products': product}
-        result = render_to_string('products/products.html', context)
-        return JsonResponse({'result': result})
+    if not baskets:
+        Basket.objects.create(user=request.user, product=product, quantity=1)
+        return HttpResponseRedirect(request.META['HTTP_REFERER'])
+    else:
+        basket = baskets.first()
+        basket.quantity += 1
+        basket.save()
+        return HttpResponseRedirect(request.META['HTTP_REFERER'])
 
 
 @login_required
