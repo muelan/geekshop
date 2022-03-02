4527eb2ca8897fb09d9d8693b8c2b7a4e15e930b
muelan <muelan@mail.ru>
2022-03-02 16:00:37 +0300

    Delete controller added

--- admins/urls.py
+++ admins/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from admins.views import index, admin_users, admin_users_create, admin_users_update
+from admins.views import index, admin_users, admin_users_create, admin_users_update, admin_users_delete
 
 app_name = 'admins'
 
@@ -9,5 +9,5 @@ urlpatterns = [
     path('users/', admin_users, name='admin_users'),
     path('users-create/', admin_users_create, name='admin_users_create'),
     path('users-update/<int:pk>/', admin_users_update, name='admin_users_update'),
-
+    path('users-delete/<int:pk>/', admin_users_delete, name='admin_users_delete'),
 ]
