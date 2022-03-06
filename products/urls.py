31dc7e892c9d140e1499ef456675150fa823f5b7
muelan <muelan@mail.ru>
2022-03-06 17:41:38 +0300

    Paginator added

--- products/urls.py
+++ products/urls.py
@@ -5,6 +5,7 @@ from products.views import products
 app_name = 'products'
 
 urlpatterns = [
-    path('', products, name='index'),
-    path('<int:category_id>/', products, name='category'),
+    path('', products, name='index'), # ../products/
+    path('category/<int:category_id>/', products, name='category'),  # ../products/category/3/
+    path('page/<int:page>/', products, name='page'),  # ../products/page/2/
 ]
--- products/views.py
+++ products/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from .models import Product, ProductCategory
 
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+
 import json
 
 # Create your views here.
@@ -11,14 +13,23 @@ def index(request):
     return render(request, 'products/index.html', context)
 
 
-def products(request, category_id=None):
+def products(request, category_id=None, page=1):
     if category_id:
         products = Product.objects.filter(category_id=category_id)
     else:
         products = Product.objects.all()
+
+    paginator = Paginator(products, 3)
+    try:
+        products_paginator = paginator.page(page)
+    except PageNotAnInteger:
+        products_paginator = paginator.page(1)
+    except EmptyPage:
+        products_paginator = paginator.page(paginator.num_pages)
+
     context = {'title': 'GeekShop - Каталог',
                'categories': ProductCategory.objects.all(),
-               'products': products,
+               'products': products_paginator,
                }
 
     return render(request, "products/products.html", context)
