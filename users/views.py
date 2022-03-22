5a332070654f436926d7f21a75ac136761596be6
muelan <muelan@mail.ru>
2022-03-22 20:50:30 +0300

    users/views to CBV

--- users/views.py
+++ users/views.py
@@ -1,77 +1,120 @@
-from django.shortcuts import render, HttpResponseRedirect
-from django.contrib import auth
-from django.urls import reverse
-from django.contrib import messages
-from django.contrib.auth.decorators import login_required
+from django.contrib.messages.views import SuccessMessageMixin
+from django.contrib.auth.views import LoginView, LogoutView
+from django.views.generic.edit import CreateView, UpdateView
+from django.urls import reverse_lazy
+# from django.contrib import messages
 
 from users.forms import UserLoginForm, UserRegistrationForm, UserProfileForm
+from users.models import User
 from baskets.models import Basket
 
 
+class CommonContextMixin:
+    title = None
 
-def login(request):
-    if request.method == 'POST':
-        form = UserLoginForm(data=request.POST)
-        if form.is_valid():
-            username = request.POST['username']
-            password = request.POST['password']
-            user = auth.authenticate(username=username, password=password)
-            if user and user.is_active:
-                auth.login(request, user)
-                return HttpResponseRedirect(reverse('index'))
-        else:
-            print(form.errors)
-    else:
-        form = UserLoginForm()
-    context = {'title': 'GeekShop - Авторизация', 'form': form}
-    return render(request, 'users/login.html', context)
+    def get_context_data(self, **kwargs):
+        context = super(CommonContextMixin, self).get_context_data(**kwargs)
+        context['title'] = self.title
+        return context
 
 
-def registration(request):
-    if request.method == 'POST':
-        form = UserRegistrationForm(data=request.POST)
-        if form.is_valid():
-            form.save()
-            messages.success(request, 'Поздравляем! Регистрация прошла успешно!')
-            return HttpResponseRedirect(reverse('users:login'))
-        else:
-            print(form.errors)
-    else:
-        form = UserRegistrationForm()
-    context = {'title': 'GeekShop - Регистрация', 'form': form}
-    return render(request, 'users/registration.html', context)
+class UserLoginView(CommonContextMixin, LoginView):
+    template_name = 'users/login.html'
+    form_class = UserLoginForm
+    title = 'GeekShop - Авторизация'
 
 
-@login_required
-def profile(request):
-    user = request.user
-    if request.method == 'POST':
-        form = UserProfileForm(instance=user, files=request.FILES, data=request.POST)
-        name1 = request.POST.get('first_name')
-        name2 = request.POST.get('last_name')
-        if form.is_valid():
-            if name1.isalpha():
-                if name2.isalpha():
-                    form.save()
-                    messages.success(request, 'Изменения внесены!')
-                    return HttpResponseRedirect(reverse('users:profile'))
-                else:
-                    messages.success(request, 'Неправильно введена фамилия')
-                    form = UserProfileForm(instance=user)
-            else:
-                messages.success(request, 'Неправильно введено имя')
-                form = UserProfileForm(instance=user)
-    else:
-        form = UserProfileForm(instance=user)
-    context = {'title': 'GeekShop - Профиль',
-               'form': form,
-               'baskets': Basket.objects.filter(user=user),
-               }
-    return render(request, 'users/profile.html', context)
+class UserRegistrationView(CommonContextMixin, SuccessMessageMixin, CreateView):
+    model = User
+    form_class = UserRegistrationForm
+    template_name = 'users/register.html'
+    success_url = reverse_lazy('users:login')
+    success_message = 'Вы успешно зарегестрировались!'
+    title = 'GeekShop - Регистрация'
 
 
-def logout(request):
-    auth.logout(request)
-    return HttpResponseRedirect(reverse('index'))
+class UserProfileView(CommonContextMixin, UpdateView):
+    model = User
+    form_class = UserProfileForm
+    template_name = 'users/profile.html'
+    title = 'GeekShop - Личный кабинет'
+
+    def get_success_url(self):
+        return reverse_lazy('users:profile', args=(self.object.id,))
+
+    def get_context_data(self, **kwargs):
+        context = super(UserProfileView, self).get_context_data(**kwargs)
+        context['baskets'] = Basket.objects.filter(user=self.object)
+        return context
+
+
+class UserLogoutView(LogoutView):
+    pass
+
+
+# def login(request):
+#     if request.method == 'POST':
+#         form = UserLoginForm(data=request.POST)
+#         if form.is_valid():
+#             username = request.POST['username']
+#             password = request.POST['password']
+#             user = auth.authenticate(username=username, password=password)
+#             if user and user.is_active:
+#                 auth.login(request, user)
+#                 return HttpResponseRedirect(reverse('index'))
+#         else:
+#             print(form.errors)
+#     else:
+#         form = UserLoginForm()
+#     context = {'title': 'GeekShop - Авторизация', 'form': form}
+#     return render(request, 'users/login.html', context)
+#
+#
+# def registration(request):
+#     if request.method == 'POST':
+#         form = UserRegistrationForm(data=request.POST)
+#         if form.is_valid():
+#             form.save()
+#             messages.success(request, 'Поздравляем! Регистрация прошла успешно!')
+#             return HttpResponseRedirect(reverse('users:login'))
+#         else:
+#             print(form.errors)
+#     else:
+#         form = UserRegistrationForm()
+#     context = {'title': 'GeekShop - Регистрация', 'form': form}
+#     return render(request, 'users/registration.html', context)
+#
+#
+# @login_required
+# def profile(request):
+#     user = request.user
+#     if request.method == 'POST':
+#         form = UserProfileForm(instance=user, files=request.FILES, data=request.POST)
+#         name1 = request.POST.get('first_name')
+#         name2 = request.POST.get('last_name')
+#         if form.is_valid():
+#             if name1.isalpha():
+#                 if name2.isalpha():
+#                     form.save()
+#                     messages.success(request, 'Изменения внесены!')
+#                     return HttpResponseRedirect(reverse('users:profile'))
+#                 else:
+#                     messages.success(request, 'Неправильно введена фамилия')
+#                     form = UserProfileForm(instance=user)
+#             else:
+#                 messages.success(request, 'Неправильно введено имя')
+#                 form = UserProfileForm(instance=user)
+#     else:
+#         form = UserProfileForm(instance=user)
+#     context = {'title': 'GeekShop - Профиль',
+#                'form': form,
+#                'baskets': Basket.objects.filter(user=user),
+#                }
+#     return render(request, 'users/profile.html', context)
+#
+#
+# def logout(request):
+#     auth.logout(request)
+#     return HttpResponseRedirect(reverse('index'))
 
 
